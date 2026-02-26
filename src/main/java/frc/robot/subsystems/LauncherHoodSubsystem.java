85b3608e949c0d2adf71713db8fd7eb58fb21dc8
jlinz <jacoblinz@gmail.com>
2026-02-26 11:15:18 -0600

    hood external encoder
    hood external encoder

--- src/main/java/frc/robot/subsystems/LauncherHoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherHoodSubsystem.java
@@ -6,9 +6,13 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.RelativeEncoder;
+//import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.*;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,67 +20,53 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class LauncherHoodSubsystem extends SubsystemBase {
-
     private final SparkMax m_motor;
+    private final SparkAbsoluteEncoder m_encoder;
     private final SparkClosedLoopController m_controller;
-    private final RelativeEncoder m_encoder;
+    private double m_targetAngleDeg = 0.0;
+    private static final double kAngleToleranceDeg = 1.0;
 
-    // The Lookup Table: Keys are Distance (meters), Values are Hood Angles (degrees)
     private final InterpolatingDoubleTreeMap m_angleTable = new InterpolatingDoubleTreeMap();
 
     public LauncherHoodSubsystem() {
         m_motor = new SparkMax(Constants.CanConstants.LauncherHoodMotorCanID, MotorType.kBrushless);
+        m_encoder = m_motor.getAbsoluteEncoder();
         m_controller = m_motor.getClosedLoopController();
-        m_encoder = m_motor.getEncoder();
 
         setupInterpolationTable();
-        
+
         SparkMaxConfig config = new SparkMaxConfig();
-        double conversionFactor = 360.0 / Constants.Launcher.kHoodGearRatio;
         
-        config.encoder
-            .positionConversionFactor(conversionFactor)
-            .velocityConversionFactor(conversionFactor / 60.0);
+        config.absoluteEncoder
+            .positionConversionFactor(360.0)
+            .velocityConversionFactor(360.0 / 60.0);
 
-        config.idleMode(IdleMode.kBrake).smartCurrentLimit(30);
+        config.idleMode(IdleMode.kBrake);
 
-        config.softLimit
-            .forwardSoftLimitEnabled(true)
-            .forwardSoftLimit(Constants.Launcher.kMaxHoodAngle)
-            .reverseSoftLimitEnabled(true)
-            .reverseSoftLimit(0);
-
-        config.closedLoop.p(0.05).outputRange(-0.4, 0.4);
+        config.closedLoop
+            // Using the most direct 2026 package path
+            .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+            .p(0.05);
 
         m_motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     private void setupInterpolationTable() {
-        // Syntax: m_angleTable.put(DistanceInMeters, HoodAngleInDegrees);
-        // Add your testing data here!
         m_angleTable.put(1.0, 10.0); 
         m_angleTable.put(2.0, 25.0);
         m_angleTable.put(3.0, 38.0);
         m_angleTable.put(5.0, 55.0);
     }
 
-    /**
-     * Calculates the distance to the target and sets the hood angle accordingly.
-     * @param robotPose Current Translation2d of the robot from Odometry
-     * @param targetPose Translation2d of the Speaker/Target
-     */
     public void setAngleFromPose(Translation2d robotPose, Translation2d targetPose) {
         double distance = robotPose.getDistance(targetPose);
-        double targetAngle = m_angleTable.get(distance);
-        
-        setAngle(targetAngle);
-        
-        SmartDashboard.putNumber("Launcher/Auto Distance", distance);
-        SmartDashboard.putNumber("Launcher/Auto Target Angle", targetAngle);
+        setAngle(m_angleTable.get(distance));
     }
 
     public void setAngle(double degrees) {
-        m_controller.setReference(degrees, SparkMax.ControlType.kPosition);
+        m_targetAngleDeg = MathUtil.clamp(degrees, 0.0, Constants.Launcher.kMaxHoodAngle);
+        // Use SparkMax.ControlType for the reference
+        m_controller.setReference(m_targetAngleDeg, SparkMax.ControlType.kPosition);
     }
 
     public void stop() {
@@ -87,8 +77,14 @@ public class LauncherHoodSubsystem extends SubsystemBase {
         return m_encoder.getPosition();
     }
 
+    public boolean isAtTarget() {
+        return Math.abs(getAngle() - m_targetAngleDeg) <= kAngleToleranceDeg;
+    }
+
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Launcher/Hood Angle", getAngle());
+        SmartDashboard.putNumber("Launcher/Hood Target Angle", m_targetAngleDeg);
+        SmartDashboard.putBoolean("Launcher/Hood At Target", isAtTarget());
     }
 }
